de11040e5316e83690c2db939d51f80ceb95d96d
Rafael Beraldo <rberaldo@cabaladada.org>
2021-09-15 23:15:30 -0300

    change organização dos diretórios

--- generate_map.R
+++ generate_map.R
@@ -7,14 +7,14 @@ require(RColorBrewer)
 require(htmlwidgets)
 
 # Diretório de trabalho
-setwd("/home/dados/LabsBrasil/BR_UF_2020")
+setwd(getwd())
 
 # Importanto arquivos de dados do IBGE com as shapes
 # Disponíveis aqui: https://www.ibge.gov.br/geociencias/organizacao-do-territorio/malhas-territoriais/15774-malhas.html?=&t=downloads
-shp <- readOGR("BR_UF_2020.shp", stringsAsFactors=FALSE, encoding="UTF-8")
+shp <- readOGR("./map-data/BR_UF_2020.shp", stringsAsFactors=FALSE, encoding="UTF-8")
 
 # Ler os arquivos com os dados dos laboratórios
-labs <- read.csv("/home/dados/LabsBrasil/BR_UF_2020/Labs.csv")
+labs <- read.csv("./data/Labs.csv")
 
 # Mesclar os shapes com os arquivos dos laboratórios by UF
 teste <- merge(shp, labs, by.x = "SIGLA_UF", by.y = "UF", duplicateGeoms = TRUE)
@@ -38,4 +38,4 @@ x <- leaflet(data = teste) %>%
 x
 
 # Salvar o mapa como HTML
-saveWidget(x, "LabsDoBrasil.html")
+saveWidget(x, "./LabsDoBrasil.html")
